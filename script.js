67a82c4c8b1db8553f3197461d46cedfd5002bae
Rob Williams <113773582+RobMWilliams@users.noreply.github.com>
2023-03-29 20:20:53 +0100

    You can now play one game over 5 rounds.

--- script.js
+++ script.js
@@ -7,80 +7,148 @@ function enlargeImage(img) {
     img.style.transform = "scale(1)"; // reset the scale of the image
     img.style.transition = "transform 0.5s"; // add a smooth transition effect
   }
-// Defines that the user has not yet chosen a "weapon"
-  let userHasChosen = false;
 
-  //User chooses an image, the weapon is chosen and no other options can be chosen.
-  const playerRockImg = document.getElementById("playerRock");
-  playerRockImg.addEventListener('click', function(){
-    if (userHasChosen != true)
-    playerChoiceImg("Rock");
-    userHasChosen = true;
-  });
+  //Variables
 
-  const playerPaperImg = document.getElementById("playerPaper");
-  playerPaperImg.addEventListener('click', function(){
-    if (userHasChosen != true)
-    playerChoiceImg("Paper");
-    userHasChosen = true;
-  });
+  let playerScore = 0;
+  let computerScore = 0;
+  let roundsPlayed = 0;
 
-  const playerScissorsImg = document.getElementById("playerScissors");
-  playerScissorsImg.addEventListener('click', function(){
-    if (userHasChosen != true)
-      playerChoiceImg("Scissors");
-    userHasChosen = true;
-  });
+  //DOM elements
 
-//The users "weapon" is displayed and logged.
-function playerChoiceImg (choice){
+  const allButtons = document.querySelectorAll("button");
+  const rockButton = document.getElementById("playerRock");
+  const paperButton = document.getElementById("playerPaper");
+  const scissorsButton = document.getElementById("playerScissors");
+  let roundText = document.getElementById("roundText");
+  let playerScoreText = document.getElementById("playerScoreText");
+  let computerScoreText = document.getElementById("computerScoreText");
+  let roundWinner = document.getElementById("winnerText");
+  let overallWinner = document.getElementById("winnerText");
 
-    if (choice === "Rock"){
-      const playerImg = document.getElementById("player");
-      const img = document.createElement("img");
-      img.src = "images/rock.png";
-      playerImg.appendChild(img);
-      console.log("Rock");
-    }
-    else if (choice === "Paper"){
-      const playerImg = document.getElementById("player");
-      const img = document.createElement("img");
-      img.src = "images/paper.png";
-      playerImg.appendChild(img);
-      console.log("Paper");
-    }
-    else if (choice === "Scissors"){
-      const playerImg = document.getElementById("player");
-      const img = document.createElement("img");
-      img.src = "images/scissors.png";
-      playerImg.appendChild(img);
-      console.log("Scissors");
-    };
 
-    return choice;
-}
- 
-function computerChoice(){
+  // Event listeners
 
-  const randomChoice = Math.floor(Math.random() * 3);
+  rockButton.addEventListener("click", () => playRound("rock"));
+  paperButton.addEventListener("click", () => playRound("paper"));
+  scissorsButton.addEventListener("click", () => playRound("scissors"));
 
-  let selectedOption;
-  switch (randomChoice) {
-    case 0:
-    selectedOption = "Rock";
-    break;
 
-    case 1:
-      selectedOption = "Paper";
-      break;
-
-    case 2:
-      selectedOption = "Scissors";
-      break;
+  //Insert chosen weapons as images
+  function insertImgs(playerChoice,computerWeapon){
+    document.getElementById("playerChoice").innerHTML=`<img class="chosenWeapons" src="images/${playerChoice}.png">`;
+    document.getElementById("computerChoice").innerHTML=`<img class="chosenWeapons" src="images/${computerWeapon}.png">`;
   }
-  console.log(`Computer choice is ${selectedOption}`)
-  return selectedOption;
 
-}
+  // Function to play one round
 
-computerChoice();
+  function playRound(playerTurn){
+
+    //confirm player choice
+    
+    console.log(`Player is ${playerTurn}`)
+
+      //increment round
+
+      roundsPlayed++
+
+      //computer choice via array
+
+      const weapons = ["rock","paper","scissors"];
+      const computerWeapon = weapons[Math.floor(Math.random() * weapons.length)];
+      console.log(`computer is ${computerWeapon}`)
+
+      //update display weapons
+      
+      insertImgs(playerTurn,computerWeapon);
+
+      //determine winner
+      
+      if(playerTurn === computerWeapon){
+        roundWinner = "Tie";
+      } else if(
+        (playerTurn === "rock" && computerWeapon === "paper") ||
+        (playerTurn === "paper" && computerWeapon === "scissors") ||
+        (playerTurn === "scissors" && computerWeapon === "rock")){
+        roundWinner = "Lost"
+      } else{
+        roundWinner = "Win"
+      }
+
+      //update score
+
+      if(roundWinner === "Win"){
+        playerScore++;
+        WinnerText.textContent = "You win that round!"
+      } else if(roundWinner === "Lost"){
+        computerScore++;
+        WinnerText.textContent = "You lost that round!"
+      } else{
+        WinnerText.textContent = "It's a tie!";
+      }
+
+      //update display
+
+      playerScoreText.textContent = playerScore;
+      computerScoreText.textContent = computerScore;
+      roundText.textContent = roundsPlayed;
+      
+      //confirm round winner
+
+      console.log(roundWinner);
+
+      //count number of rounds and end game at round 5
+
+      if(roundsPlayed === 5){
+        endGame(playerScore,computerScore);
+        return;
+        
+      }
+
+      return;
+  }
+
+  // function to end the game
+
+  function endGame(playerScore, computerScore){
+    
+    //disable all buttons
+      disableButtons();  
+    
+    //determine game outcome
+
+      if(playerScore > computerScore){
+        overallWinner = "player";
+      } else{
+        overallWinner = "computer";
+      }
+
+      //update score with winner
+
+      if(overallWinner === "player"){
+        WinnerText.textContent = "You win the game!";
+      } else{
+        WinnerText.textContent = "You lose the game!"
+      }
+
+
+
+  }
+
+  
+    function disableButtons() {
+      rockButton.disabled = true;
+      paperButton.disabled = true;
+      scissorsButton.disabled = true;
+      rockButton.removeEventListener("click", playRound);
+      paperButton.removeEventListener("click", playRound);
+      scissorsButton.removeEventListener("click", playRound);
+    }
+    
+      console.log(rockButton);
+      console.log(paperButton);
+      console.log(scissorsButton);
+    
+  
+
+  
